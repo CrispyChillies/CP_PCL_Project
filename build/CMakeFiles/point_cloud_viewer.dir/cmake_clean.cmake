3ee6f91a51bd1f6377d51ee461031586c0709734
CrispyChillies <phamnguyenhailongct@gmail.com>
2024-12-04 19:36:03 +0700

    Add file to github

---
+++ build/CMakeFiles/point_cloud_viewer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/point_cloud_viewer.dir/main.cpp.o"
+  "CMakeFiles/point_cloud_viewer.dir/main.cpp.o.d"
+  "point_cloud_viewer"
+  "point_cloud_viewer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/point_cloud_viewer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
